8b4451dc0fbf7a671c49cfaa9dca6274eeb175b9
noaquare <noasquare@qq.com>
2019-06-22 22:46:11 +0800

    用户去重解决

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -13,11 +13,11 @@ export class UserService {
     ){}
 
     async store(data: UserDto){
-        // const {name} = data; 
-        // const user = this.userRepository.findOne({name});
-        // if(user){
-        //     throw new BadRequestException('用户已经存在了ne');
-        // }
+        const {name} = data; 
+        const user = await this.userRepository.findOne({name});
+        if(user){
+            throw new BadRequestException('用户已经存在了');
+        }
         const entity = await this.userRepository.create(data);
         await this.userRepository.save(entity);
         return entity;
